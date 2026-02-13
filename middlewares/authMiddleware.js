6df42df0b8d83ba5e059ed6c52d1acfe5428e775
Indhu-pv <pindhu2605@gmail.com>
2026-02-13 22:25:24 +0530

    Fix typo in authMiddleware error message

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ module.exports = async (req, res, next) => {
   try {
     const token = req.headers["authorization"].split(" ")[1];
     JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
-      if (err) {
+      if (err) { 
         return res.status(200).send({
           message: "Auth Fialed",
           success: false,
