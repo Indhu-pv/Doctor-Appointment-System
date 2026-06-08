56925098b0f1eb232fd8818fc6e69b370cf15fd1
Indhu-pv <pindhu2605@gmail.com>
2026-06-08 22:24:17 +0530

    last commits
    Removed comments for clarity in admin routes.

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -8,13 +8,10 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-//GET METHOD || USERS
 router.get("/getAllUsers", authMiddleware, getAllUsersController);
 
-//GET METHOD || DOCTORS
 router.get("/getAllDoctors", authMiddleware, getAllDoctorsController);
 
-//POST ACCOUNT STATUS
 router.post(
   "/changeAccountStatus",
   authMiddleware,
